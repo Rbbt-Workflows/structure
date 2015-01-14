703df52390b72aeba2b35ccee93a888c58216e31
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-01-14 19:15:51 +0100

    nil

--- www/views/public/js/secondary_structure.js
+++ www/views/public/js/secondary_structure.js
@@ -3,15 +3,19 @@ $.widget("rbbt.secondary_structure_tool", {
   options: { },
 
   _create: function() {
-    this.element.addClass('secondary_structure_tool_init');
-    this.options.sequence = this.element.find('.sequence_tool');
-    this.options.svg = this.element.find('.isoform_svg_tool');
+    var tool = this;
+    tool.element.addClass('secondary_structure_tool_init');
+    tool.options.sequence = tool.element.find('.sequence_tool');
+    tool.options.svg = tool.element.find('.isoform_svg_tool');
   },
 
   _redirect_function: function(name){
     var args = arguments;
-    this.options.sequence.sequence_tool(name, args[1], args[2], args[3]);
-    this.options.svg.isoform_svg_tool(name, args[1], args[2], args[3]);
+    var tool = this;
+    require_js(["/js/sequence.js", "/js/isoform_svg.js"], function(){
+      tool.options.sequence.sequence_tool(name, args[1], args[2], args[3]);
+      tool.options.svg.isoform_svg_tool(name, args[1], args[2], args[3]);
+    })
   },
 
   clear: function(){
