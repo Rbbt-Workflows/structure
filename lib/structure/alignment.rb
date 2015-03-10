fdc1256a8b0b150cdb85f6a36bb5a383e336615c
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-03-10 13:25:46 +0100

    nil

--- lib/structure/alignment.rb
+++ lib/structure/alignment.rb
@@ -5,7 +5,7 @@ module Structure
   ALIGNMENTS = cache_dir.alignment_maps.find
   Open.repository_dirs << ALIGNMENTS unless Open.repository_dirs.include? ALIGNMENTS
   def self.alignment_map(alignment_source, alignment_target)
-    Persist.persist("Alignment maps", :marshal, :dir => ALIGNMENTS, :lock => {:max_age => 0, :suspend => 0, :refresh => 0}, :other => {:alignment_source => alignment_source, :alignment_target => alignment_target}) do
+    Persist.persist("Alignment maps", :marshal, :dir => ALIGNMENTS, :lock => {:max_age => 0, :suspend => 0, :refresh => 0}, :other => {:alignment_source => alignment_source, :alignment_target => alignment_target, :version => "v4"}) do
       map = {}
 
       offset_source, alignment_source = alignment_source.match(/^(_*)(.*)/).values_at( 1, 2)
@@ -13,14 +13,25 @@ module Structure
 
       gaps_source = 0 
       gaps_target = 0
+      miss_match = 0
       alignment_source.chars.zip(alignment_target.chars).each_with_index do |p,i|
         char_source, char_target = p
         gaps_source += 1 if char_source == '-'
         gaps_target += 1 if char_target == '-'
-        map[i + 1 + offset_source.length - gaps_source] = i + 1 + offset_target.length - gaps_target if char_source == char_target  and char_source != "-"
+        source_pos = i + 1 + offset_source.length - gaps_source
+        target_pos = i + 1 + offset_target.length - gaps_target
+        if char_source != char_target or char_source == "-"
+          miss_match += 1
+        else
+          map[source_pos] = target_pos 
+        end
       end
 
-      map
+      if miss_match + gaps_source > alignment_source.length.to_f / 2
+        {}
+      else
+        map
+      end
     end
   end
 
