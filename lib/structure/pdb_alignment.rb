ab6064ef8c82bf707995ef22179cdf04501bc621
Miguel Vazquez <miguel.vazquez@cnio.es>
2015-11-23 21:25:33 +0100

    nil

--- lib/structure/pdb_alignment.rb
+++ lib/structure/pdb_alignment.rb
@@ -92,7 +92,7 @@ module Structure
   def self.neighbour_map_job(pdb, pdbfile, distance)
     Misc.insist do
       begin
-        Persist.persist("Neighbour map", :yaml, :persist => true, :dir => NEIGHBOUR_MAP, :other => {:pdb => pdb, :pdbfile => pdbfile, :distance => distance}) do  |filename|
+        Persist.persist("Neighbour map", :yaml, :dir => NEIGHBOUR_MAP, :other => {:pdb => pdb, :pdbfile => pdbfile, :distance => distance}) do  |filename|
           job = Structure.job(:neighbour_map, "PDB Neighbours", :pdb => pdb, :pdbfile => pdbfile, :distance => distance)
           job.run.to_hash
         end
