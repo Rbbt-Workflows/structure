5e0d435d445c3ec4546c84cdc7db9670c3a957fc
Miguel Vazquez <miguel.vazquez@cnio.es>
2016-01-21 15:27:32 +0100

    nil

--- lib/structure/workflow/wizard.rb
+++ lib/structure/workflow/wizard.rb
@@ -114,6 +114,7 @@ module Structure
     all_annotations.attach(all_annotations_n, :fields => all_annotations_n.fields.reject{|f| f =~ /Mutated Isoform/})
 
     all_annotations.namespace = organism
+
     all_annotations
   end
   export_asynchronous :wizard
