214033f6c24dee1c75ee5bdb53d28f18cc2dc67d
mimadrid <mimadrid@ucm.es>
2017-07-24 16:16:14 +0200

    Changes:   - Rewrite NGL wrapper   - Add interaction with the sequences viewer   - Split features by type   - Redesing a bit the UI

--- www/views/public/js/rbbt.protein_tool.js
+++ www/views/public/js/rbbt.protein_tool.js
@@ -16,11 +16,18 @@ rbbt.sequence.mark_position = function(element, position, color){
 
 rbbt.svg = {}
 
-rbbt.svg.clear = function(element){
+rbbt.svg.clear = function(element, layer=''){
   var svg = element.find('svg');
-  var vlines = svg.find('.rbbt-vline')
+  var vlines;
+  var vregions;
+  if (layer) {
+    vlines = svg.find('.rbbt-vline.' + layer)
+    vregions = svg.find('.rbbt-region.' + layer)
+  } else {
+    vlines = svg.find('.rbbt-vline')
+    vregions = svg.find('.rbbt-region')
+  }
   vlines.remove()
-  var vregions = svg.find('.rbbt-region')
   vregions.remove()
 }
 
@@ -29,10 +36,10 @@ rbbt.svg.position_offset = function(element, position){
   var width  = parseInt(svg.attr('width'));
   var start  = parseInt(svg.find('rect.ac').attr('x'));
   var seq_len  = parseInt(element.attr('data-sequence_length'));
-  return start + position * (width - start)/seq_len 
+  return start + position * (width - start)/seq_len
 }
 
-rbbt.svg.mark_position = function(element, position, color){
+rbbt.svg.mark_position = function(element, position, color, layer=''){
   if (undefined === color) color = 'red';
   var svg = element.find('svg');
 
@@ -45,36 +52,40 @@ rbbt.svg.mark_position = function(element, position, color){
   line.setAttributeNS(null, "y1", 5);
   line.setAttributeNS(null, "x2", offset);
   line.setAttributeNS(null, "y2", height - 5);
-  line.setAttributeNS(null, "class", 'rbbt-vline');
+  line.setAttributeNS(null, "class", 'rbbt-vline' + (layer ? ' ' + layer : ''));
   line.setAttributeNS(null, "style", "stroke:" + color + ";opacity:0.5;stroke-width:1;");
 
   svg.append(line);
 }
 
-rbbt.svg.mark_positions = function(element, positions, color){
+rbbt.svg.mark_positions = function(element, positions, color, layer=''){
   for (var i = 0; i < positions.length; i++){
-    var position = positions[i]
+    var position = positions[i];
     var seq_len  = parseInt(element.attr('data-sequence_length'));
     jitter = Math.ceil(seq_len / 1000) + 2;
     position = position + Math.floor(Math.random() * 2 * jitter) -jitter + 1
-    rbbt.svg.mark_position(element, position, color)
+    if (Array.isArray(color)) {
+      rbbt.svg.mark_position(element, position, color[i], layer);
+    } else {
+      rbbt.svg.mark_position(element, position, color, layer);
+    }
   }
 }
 
-rbbt.svg.mark_region = function(element, first, last, color){
+rbbt.svg.mark_region = function(element, first, last, color, layer=''){
   var svg = element.find('svg')
-  var width  = parseInt(svg.attr('width'));
+  var width = parseInt(svg.attr('width'));
   var height  = parseInt(svg.attr('height'));
-  var start  = parseInt(svg.find('rect.ac').attr('x'));
+  var start = parseInt(svg.find('rect.ac').attr('x'));
   var seq_len  = parseInt(element.attr('data-sequence_length'));
 
-  var offset_start = rbbt.svg.position_offset(element, first)
-  var offset_end = rbbt.svg.position_offset(element, last)
+  var offset_start = rbbt.svg.position_offset(element, first);
+  var offset_end = rbbt.svg.position_offset(element, last);
 
   var rect =  document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
   rect.setAttributeNS(null, "x", offset_start);
-  rect.setAttributeNS(null, "class", 'rbbt-region');
+  rect.setAttributeNS(null, "class", 'rbbt-region' + (layer ? ' ' + layer : ''));
   rect.setAttributeNS(null, "y", 10);
   rect.setAttributeNS(null, "width",  offset_end - offset_start);
   rect.setAttributeNS(null, "height", height - 30);
@@ -83,50 +94,25 @@ rbbt.svg.mark_region = function(element, first, last, color){
   svg.append(rect);
 }
 
-rbbt.svg.mark_aligned_region = function(element, map, color){
+rbbt.svg.mark_aligned_region = function(element, map, color, layer=''){
   if (undefined === color) color = 'blue'
 
     var positions = []
     for(seq_pos in map){
-      positions.push(parseInt(seq_pos))
+      positions.push(parseInt(seq_pos));
     }
 
     positions = positions.sort();
 
-    var last = -1
+    var last = -1;
     var start = -1;
     for (var i = 0; i < positions.length; i++){
       if (positions[i] != last + 1){
-        if (start != -1) { rbbt.svg.mark_region(element, start, last, color); }
-        start = positions[i]
+        if (start != -1) { rbbt.svg.mark_region(element, start, last, color, layer); }
+        start = positions[i];
       }
-      last = positions[i]
+      last = positions[i];
     }
-    if (start != -1) { rbbt.svg.mark_region(element, start, last, color); }
+    if (start != -1) { rbbt.svg.mark_region(element, start, last, color, layer); }
 }
-
-
-rbbt.jmol = {}
-rbbt.jmol.clear = function(element){
-  element.jmol_tool('clear')
-}
-
-rbbt.jmol.loaded = function(element){
-  return(element.jmol_tool('is_pdb_loaded'));
-}
-
-rbbt.jmol.mark_position = function(element, position, color){
-  if (undefined === color) color = 'red';
-  if (rbbt.jmol.loaded(element)){element.jmol_tool('mark_position', position, color)}
-}
-
-rbbt.jmol.mark_positions = function(element, positions, color){
-  if (undefined === color) color = 'red';
-  if (rbbt.jmol.loaded(element)){element.jmol_tool('mark_positions', positions, color)}
-}
-
-rbbt.jmol.color_positions = function(element, incidence){
-  if (rbbt.jmol.loaded(element)){element.jmol_tool('color_mutation_density', incidence)}
-}
-
 
